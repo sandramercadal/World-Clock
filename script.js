5a89d4a2b2a0d880b3ebab06b4adbe4f883b8757
sandramerc <144231118+sandramercadal@users.noreply.github.com>
2023-11-14 14:04:25 +0000

    Added Moment js and moment timezone. Changed Florida to Orlando.

---
+++ script.js
@@ -0,0 +1,14 @@
+function updateTime() {
+  let parisElement = document.querySelector("#paris");
+  let parisDateElement = parisElement.querySelector(".date");
+  let parisTimeElement = parisElement.querySelector(".time");
+  let parisTime = moment().tz("Europe/Paris");
+
+  parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
+
+  parisTimeElement.innerHTML = parisTime.format(
+    "h:mm:ss [<small>] A [</small]"
+  );
+  updateTime();
+  setInterval(updateTime, 1000);
+}
