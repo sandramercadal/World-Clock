632a03a624045b5e05479cbd66cb97eddebc1358
sandramerc <144231118+sandramercadal@users.noreply.github.com>
2023-11-14 14:26:47 +0000

    Adding in JS id's for each location.

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 function updateTime() {
   let parisElement = document.querySelector("#paris");
+
   let parisDateElement = parisElement.querySelector(".date");
   let parisTimeElement = parisElement.querySelector(".time");
   let parisTime = moment().tz("Europe/Paris");
 
-  parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
+  parisDateElement.innerHTML = parisTime.format("MMMM	Do YYYY");
+  parisTimeElement.innerHTML = parisTime.format("h:mm:ss [<small>]A[</small>]");
 
-  parisTimeElement.innerHTML = parisTime.format(
-    "h:mm:ss [<small>] A [</small]"
-  );
   updateTime();
   setInterval(updateTime, 1000);
 }
